d122e298ccae179e987279ef335fb26437220c08
Alex Running <runniale000@gmail.com>
2022-06-07 13:56:29 -0500

    added more good stuff

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,9 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector("#username-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
+    const username = document.querySelector("#username").value.trim();
+    const password = document.querySelector("#password").value.trim();
+    const email = document.querySelector("#email").value.trim();
     if (username && password) {
       const response = await fetch("/api/users", {
         method: "POST",
@@ -25,5 +25,5 @@ const signupFormHandler = async (event) => {
   };
   
   document
-    .querySelector(".signup-form")
+    .querySelector(".signupForm")
     .addEventListener("submit", signupFormHandler);
