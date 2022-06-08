1890598b8519c35ffab331be45b468ce1a27f1e9
unknown <agoetz4407@gmail.com>
2022-06-07 21:41:07 -0500

    added TODO comments

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,10 +1,12 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
+    //TODO: grab location for user
     const dogName = document.getElementById('dogName').value.trim();
     const dogBreed = document.getElementById('dogBreed').value.trim();
     const dogAge = document.getElementById('dogAge').value.trim();
     const sex = document.querySelector("[name='sex']:checked").value;
+    //TODO: change username to name
     const username = document.querySelector("#username").value.trim();
     const password = document.querySelector("#password").value.trim();
     const email = document.querySelector("#email").value.trim();
